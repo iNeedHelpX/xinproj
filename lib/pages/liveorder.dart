d313d0b773da2da3e54eff9951ee5558cd4079e0
Julia Pak <jpak0444@gmail.com>
2022-06-04 20:27:28 -0400

    pages

---
+++ lib/pages/liveorder.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LiveOrder extends StatefulWidget {
+  LiveOrder({Key? key}) : super(key: key);
+
+  @override
+  State<LiveOrder> createState() => _LiveOrderState();
+}
+
+class _LiveOrderState extends State<LiveOrder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/walletpay.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PayWallet extends StatefulWidget {
+  PayWallet({Key? key}) : super(key: key);
+
+  @override
+  State<PayWallet> createState() => _PayWalletState();
+}
+
+class _PayWalletState extends State<PayWallet> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
